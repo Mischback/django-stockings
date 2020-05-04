798434b849009af56c381cd145188f78debec90b
Mischback <dev@mischback.de>
2020-05-04 18:25:01 +0200

    Added base class for tests without signal handlers

--- tests/util/testcases.py
+++ tests/util/testcases.py
@@ -1,10 +1,58 @@
 """Provides app-specific classes for test cases."""
 
 # Django imports
+from django.apps.registry import apps
+from django.db.models.signals import post_save
 from django.test import TestCase
 
 
-class StockingsTestCase(TestCase):
+class StockingsTestCaseBase(TestCase):
     """Base class for all app-specific tests."""
 
-    pass
+    @classmethod
+    def _disconnect_signal_callbacks(cls):
+        """Disconnect all signal callbacks for testing."""
+
+        post_save.disconnect(
+            apps.get_model('stockings.PortfolioItem').callback_perform_trade,
+            sender=apps.get_model('stockings.Trade'),
+            dispatch_uid='STOCKINGS_portfolioitem_trade',
+        )
+
+        post_save.disconnect(
+            apps.get_model('stockings.PortfolioItem').callback_price_update,
+            sender=apps.get_model('stockings.StockItem'),
+            dispatch_uid='STOCKINGS_portfolioitem_price',
+        )
+
+    @classmethod
+    def _reconnect_signal_callbacks(cls):
+        """Reconnect all signal callbacks for testing."""
+
+        post_save.connect(
+            apps.get_model('stockings.PortfolioItem').callback_perform_trade,
+            sender=apps.get_model('stockings.Trade'),
+            dispatch_uid='STOCKINGS_portfolioitem_trade',
+        )
+
+        post_save.connect(
+            apps.get_model('stockings.PortfolioItem').callback_price_update,
+            sender=apps.get_model('stockings.StockItem'),
+            dispatch_uid='STOCKINGS_portfolioitem_price',
+        )
+
+
+class StockingsTestCase(StockingsTestCaseBase):
+    """This class supports testing without the app's signals."""
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls._disconnect_signal_callbacks()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._reconnect_signal_callbacks()
+
+        super().tearDownClass()
--- tests/util/urls_dev.py
+++ tests/util/urls_dev.py
@@ -2,7 +2,7 @@
 a tox-based environment."""
 
 # Django imports
-from django.conf.urls import include, url
+from django.conf.urls import include, url  # noqa: F401
 from django.contrib import admin
 
 urlpatterns = [
