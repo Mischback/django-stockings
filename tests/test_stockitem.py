3264b45108ff3eb615ab132b3342309c11104335
Mischback <dev@mischback.de>
2020-05-22 10:54:20 +0200

    Added test for property latest_price

--- tests/test_stockitem.py
+++ tests/test_stockitem.py
@@ -32,5 +32,21 @@ class StockItemTest(StockingsTestCase):
         with self.assertRaises(AttributeError):
             del a.currency
 
-    def test_property_latest_price(self):
-        pass
+    @mock.patch("stockings.models.stock.StockItemPrice")
+    def test_property_latest_price(self, mock_price_object):
+
+        # get a `StockItem` object
+        a = StockItem()
+
+        # getter
+        b = a.latest_price
+        self.assertTrue(mock_price_object.get_latest_price.called)
+        self.assertEqual(b, mock_price_object.get_latest_price.return_value)
+
+        # setter
+        a.latest_price = 1337
+        self.assertTrue(mock_price_object.set_latest_price.called)
+
+        # deleting should not be possible
+        with self.assertRaises(AttributeError):
+            del a.latest_price
