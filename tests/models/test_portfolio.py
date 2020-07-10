3017cbefb8989c10518d97ee8a944eaef544029b
Mischback <dev@mischback.de>
2020-07-10 11:34:16 +0200

    Added tests for Portfolio.currency (setter, deleter)

--- tests/models/test_portfolio.py
+++ tests/models/test_portfolio.py
@@ -23,3 +23,32 @@ class PortfolioTest(StockingsTestCase):
         a = Portfolio()
 
         self.assertEqual(a._currency, a.currency)
+
+    @mock.patch("stockings.models.portfolio.Portfolio.save")
+    @mock.patch("stockings.models.portfolio.Portfolio.portfolioitem_set")
+    def test_property_currency_set(self, mock_portfolioitem_set, mock_save):
+        """Property's setter updates associated `PortfolioItem` objects."""
+        # set up the mock
+        mock_item = mock.MagicMock()
+        mock_portfolioitem_set.iterator.return_value.__iter__.return_value = iter(
+            [mock_item]
+        )
+
+        # get a Portfolio object
+        a = Portfolio()
+
+        a.currency = "FOO"
+
+        mock_item._apply_new_currency.assert_called_with("FOO")
+        self.assertTrue(mock_item.save.called)
+
+        self.assertEqual(a._currency, "FOO")
+        self.assertTrue(mock_save.called)
+
+    def test_property_currency_del(self):
+        """Property can not be deleted."""
+        # get a Portfolio object
+        a = Portfolio()
+
+        with self.assertRaises(AttributeError):
+            del a.currency
