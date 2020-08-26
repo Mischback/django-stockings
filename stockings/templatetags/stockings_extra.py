43a20fd302f3d012879cf1bf2fc8a6a2aaccb768
Mischback <dev@mischback.de>
2020-08-26 21:18:37 +0200

    Changed format_money() to money_locale()
    Additionally, the babel import was made optional.

--- stockings/templatetags/stockings_extra.py
+++ stockings/templatetags/stockings_extra.py
@@ -1,26 +1,74 @@
 """Provides an app-specific collection of templatetags."""
 
+# Python imports
+import logging
+
 # Django imports
 from django import template
 
-# external imports
-from babel.numbers import format_currency
-
-# app imports
-from stockings.i18n import get_current_locale
+logger = logging.getLogger(__name__)
 
 register = template.Library()
 """Required module-level variable to build a templatetag library."""
 
 
-@register.simple_tag
-def format_money(money_instance):
-    """Format a money value (including currency) based on the user's locale.
+def _internal_fallback_format_currency(amount, currency):
+    """Format a money value (including currency).
+
+    This is an internal fallback function, that is utilised if the
+    :func:`~stockings.templatetags.stockings_extra.money_locale` templatetag is
+    used without having `Babel` installed.
+
+    Parameters
+    ----------
+    amount : decimal.Decimal
+    currency : str
 
     Returns
     -------
     formatted money value : str
     """
+    return "{} {}".format(currency, amount)
+
+
+@register.simple_tag
+def money_locale(money_instance):
+    """Format a money value (including currency) based on the user's locale.
+
+    Parameters
+    ----------
+    money_instance : stockings.data.StockingsMoney
+
+    Returns
+    -------
+    formatted money value : str
+
+    Notes
+    -----
+    The function will try to use `Babel` to provide a locale-aware formatting
+    of the money's value including its currency.
+
+    However, if `Babel` is not available, it will fall back to an internal
+    formatting function
+    :func:`stockings.templatetags.stockings_extra._internal_fallback_format_currency`.
+
+    If locale-aware formatting is not required or desired,
+    :func:`stockings.templatetags.stockings_extra.money` may be used to control
+    the format of :class:`stockings.data.StockingsMoney` instances.
+    """
+    try:
+        # Actually, only the babel import may fail, but `get_current_locale` is
+        # only required, if `format_currency` is available.
+        from babel.numbers import format_currency
+        from stockings.i18n import get_current_locale
+    except ImportError:
+        logger.info(
+            "`Babel` could not be loaded. Falling back to an internal formatting function."
+        )
+        return _internal_fallback_format_currency(
+            money_instance.amount, money_instance.currency
+        )
+
     return format_currency(
         money_instance.amount, money_instance.currency, locale=get_current_locale()
     )
@@ -39,5 +87,10 @@ def money(money_instance):  # noqa: D205, D400
     -----
     The actual formatting is done with a (modifiable) template,
     ``stockings/money_instance.html``.
+
+    If the formatting is required to be locale-aware,
+    :func:`stockings.templatetags.stockings_extra.money_locale` may be used (of
+    course, that `templatetag` may aswell be used in the template, used by this
+    tag, for even more flexibility).
     """
     return {"money": money_instance}
