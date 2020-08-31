fab0f6b83bead4b6c47dbafcb49e145f4514387e
Mischback <dev@mischback.de>
2020-08-31 10:38:27 +0200

    Added tests for templatetag money
    The isort configuration was modified (in tox.ini) to match black's coding style

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -1,14 +1,25 @@
 """Tests for package `stockings.templatetags`."""
 
 # Python imports
-from unittest import mock, skip  # noqa
+from unittest import (  # noqa
+    mock,
+    skip,
+)
 
 # Django imports
-from django.test import override_settings, tag  # noqa
+from django.template import (
+    Context,
+    Template,
+)
+from django.test import (  # noqa
+    override_settings,
+    tag,
+)
 
 # app imports
-from stockings.templatetags.stockings_extra import (  # noqa: isort
+from stockings.templatetags.stockings_extra import (
     _internal_fallback_format_currency,
+    money,
 )
 
 # app imports
@@ -30,3 +41,23 @@ class StockingsExtraTest(StockingsTestCase):
 
         # some more rounding
         self.assertEqual("EUR 13.37", _internal_fallback_format_currency(13.368, "EUR"))
+
+    def test_money_simple(self):
+        """The parameter is provided as context.
+
+        This test does not actually do something relevant, because it only
+        directly targets the templatetags code. The real magic however is
+        happening in the background by Django, meaning the rendering and
+        inclusion of the template.
+        """
+        self.assertEqual(money("foo"), {"money": "foo"})
+
+    def test_money_full(self):
+        """The tag includes the template and renders it using the provided context."""
+        dummy_template = Template("{% load stockings_extra %}" "{% money foo %}")
+        context = Context({"foo": "foo"})
+
+        with self.assertRaises(AttributeError):
+            self.assertTemplateUsed(
+                dummy_template.render(context), "stockings/money_instance.html"
+            )
