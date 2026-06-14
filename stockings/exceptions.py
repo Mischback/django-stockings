8721b6e4c24f58e5180ad7706deec31797a26d0b
Mischback <dev@mischback.de>
2026-06-14 21:35:49 +0200

    Add app-specific base exceptions

---
+++ stockings/exceptions.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2026 Mischback
+# SPDX-License-Identifier: MIT
+# SPDX-FileType: SOURCE
+
+"""App-specific exceptions."""
+
+
+class StockingsException(Exception):
+    """Base class for all app-specific exceptions."""
+
+
+class StockingsModelException(StockingsException):
+    """Base class for all app-specific and model-related exceptions."""
