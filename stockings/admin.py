eeb4df5a81d0e46817e5b1a8c1a6fe9c6f521788
Mischback <dev@mischback.de>
2026-06-14 22:10:48 +0200

    Integrate Portfolio into Django's admin interface

---
+++ stockings/admin.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2026 Mischback
+# SPDX-License-Identifier: MIT
+# SPDX-FileType: SOURCE
+
+"""Integrates the app's models into Django's admin interface."""
+
+# Django imports
+from django.contrib import admin
+
+# app imports
+from stockings.models.portfolio import Portfolio
+
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):  # noqa: D101
+    pass
