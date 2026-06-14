49fc635141557f69c93389736613494e16bc38a3
Mischback <dev@mischback.de>
2026-06-14 22:04:18 +0200

    Add Portfolio model

---
+++ stockings/models/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2026 Mischback
+# SPDX-License-Identifier: MIT
+# SPDX-FileType: SOURCE
+
+"""App-specific models."""
+
+# app imports
+from stockings.models.portfolio import Portfolio  # noqa: F401
