c928b00390f60156805ddbf22de31dd1c164d328
Mischback <dev@mischback.de>
2026-06-27 20:25:29 +0200

    Provide management views for yfinance_simple

--- tests/util/urls_dev.py
+++ tests/util/urls_dev.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("stockings.urls")),
+    path("yfinance/", include("stockings.contrib.provider.yfinance_simple.urls")),
 ]
 
 try:
