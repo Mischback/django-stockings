ff05e811a2e424cf0c31b5b27779711740fc296a
Mischback <dev@mischback.de>
2026-07-03 23:24:52 +0200

    Add custom templatetag to format StockingsMoney

---
+++ stockings/templatetags/stockings_tags.py
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: 2026 Mischback
+# SPDX-License-Identifier: MIT
+# SPDX-FileType: SOURCE
+
+"""App-specific templatetags."""
+
+# Python imports
+import logging
+
+# Django imports
+from django import template
+from django.utils.translation import get_language
+
+# external imports
+from babel.numbers import format_currency
+
+# app imports
+from stockings.data import StockingsMoney
+
+logger = logging.getLogger(__name__)
+
+register = template.Library()
+
+
+@register.filter(name="format_money")
+def format_money(value):
+    """Format :class:`~stockings.data.StockingsMoney` instances."""
+    # ONLY apply to StockingsMoney
+    if not isinstance(value, StockingsMoney):
+        return value
+
+    current_locale = get_language() or "en_US"
+    babel_locale = current_locale.replace("-", "_")
+
+    try:
+        return format_currency(
+            value.amount,
+            value.currency,
+            locale=babel_locale,
+        )
+    except Exception:
+        logger.warn("'format_currency()' failed!")
+        return "{} {}".format(value.amount, value.currency)
